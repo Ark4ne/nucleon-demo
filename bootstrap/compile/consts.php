e44fa4fb569e94f62d1858fd8d26f6a003bf94d6
Ark4ne <gui.allegret@gmail.com>
2019-02-19 20:06:38 +0100

    fix version php72 phalcon3, composer update, neutrino1.3.0-RC1

--- bootstrap/compile/consts.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BASE_PATH', __DIR__ . '/../../');
-define('APP_ENV', ($_ = getenv('APP_ENV')) === false ? 'development' : $_);
-define('APP_DEBUG', true);
-define('DB_DRIVER', '\\Phalcon\\Db\\Adapter\\Pdo\\Postgresql');
-define('DB_URL', getenv('DATABASE_URL'));
-define('SESSION_ID', 'heroku-session-id');
